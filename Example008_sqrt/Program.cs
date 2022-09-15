9699c823b0b02841b4b4e838de1c180434476c4b
Hitahci <stefan36rus@gmail.com>
2022-09-16 00:27:59 +0300

    8 and 9 exm

---
+++ Example008_sqrt/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+int A = 5;
+int B = 25;
+
+if(A*A==B)
+{
+    Console.WriteLine("Да");
+}
+Console.WriteLine("Нет");
---
+++ Example009_kalendar/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+int Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday  = 7;
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (Monday == a) Console.WriteLine("Понедельник");
+if (Tuesday == a) Console.WriteLine("Вторник");
+if (Wednesday == a) Console.WriteLine("Среда");
+if (Thursday == a) Console.WriteLine("Четверг");
+if (Friday == a) Console.WriteLine("Пятница");
+if (Saturday == a) Console.WriteLine("Суббота");
+if (Sunday == a) Console.WriteLine("Воскресенье");
