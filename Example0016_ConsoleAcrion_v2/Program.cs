1359e71c9b9d279daa3c3a3717a03c089199f896
amaikiri <stefan36rus@gmail.com>
2022-10-08 01:26:13 +0300

    time ready

--- Example0016_ConsoleAcrion_v2/Program.cs
+++ Example0016_ConsoleAcrion_v2/Program.cs
@@ -1,56 +1,139 @@
 ﻿
-class position
+// class position
+// {
+//     int x = 1;
+//     int y = 1;
+// }
+// class Program
+// {
+//     static void Main()
+//     {
+//         // Отрисовка поля 
+//         void map(string[,] mass)
+//         {
+//             for (int i = 0; i < 12; i++)
+//             {
+//                 for (int j = 0; j < 12; j++)
+//                 {
+//                     Console.Write(mass[i][j]);
+//                 }
+//                 Console.WriteLine();
+//             }
+//         }
+//         // W A S D 
+
+//         string[,] numbers =
+// //         {
+//         { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
+//         { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
+//         };
+
+//         //map(numbers);
+
+//         // for (int d = 0; d < 10; d++)
+//         // {
+//         //     var move = Console.ReadKey();
+//         //     // if (move = "w") position.y++;
+//         //     // if (move = "s") position.y--;
+//         //     // if (move = "a") position.x--;
+//         //     // if (move = "d") position.x++;
+//         //     map();
+
+
+
+//         // }
+//     }
+// }
+
+
+Console.Clear();
+int x;
+int y;
+
+void printSimbol(int posX, int posY)
 {
-    int x = 1;
-    int y = 1;
+    Console.SetCursorPosition(posX, posY);
+    Console.Write("@");
 }
-class Program
+void moveSimbol()
 {
-    static void Main()
+    int DownArrow = 0;
+    int RigthArrow = 0;
+    while (true)
     {
-        // Отрисовка поля 
-        void map(string[,] mass)
+        ConsoleKeyInfo cki;
+        // Console.TreatControlCAsInput = true;
+        cki = Console.ReadKey();
+        //Console.WriteLine(cki.Key.ToString());
+        if (cki.Key == ConsoleKey.Escape)   break;
+        if (cki.Key == ConsoleKey.RightArrow && RigthArrow < 50)
         {
-            for (int i = 0; i < 12; i++)
-            {
-                for (int j = 0; j < 12; j++)
-                {
-                    Console.Write(mass[i][j]);
-                }
-                Console.WriteLine();
-            }
+            Console.Clear();
+            printBoard();
+            RigthArrow++;
+            printSimbol(RigthArrow, DownArrow);
         }
-        // W A S D 
-
-        string[,] numbers =
+        if (cki.Key == ConsoleKey.DownArrow && DownArrow < 6)
         {
-        { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X" },
-        { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" },
-        };
 
-        //map(numbers);
-
-        // for (int d = 0; d < 10; d++)
-        // {
-        //     var move = Console.ReadKey();
-        //     // if (move = "w") position.y++;
-        //     // if (move = "s") position.y--;
-        //     // if (move = "a") position.x--;
-        //     // if (move = "d") position.x++;
-        //     map();
-        
-
-
-        // }
+            Console.Clear();
+            printBoard();
+            DownArrow++;
+            printSimbol(RigthArrow, DownArrow);
+        }
+        if (cki.Key == ConsoleKey.LeftArrow && RigthArrow > 0)
+        {
+            Console.Clear();
+            printBoard();
+            RigthArrow--;
+            printSimbol(RigthArrow, DownArrow);
+        }
+        if (cki.Key == ConsoleKey.UpArrow && DownArrow > 0)
+        {
+            Console.Clear();
+            printBoard();
+            DownArrow--;
+            printSimbol(RigthArrow, DownArrow);
+        }
     }
 }
+
+
+void printBoard()
+{
+    String[,] s ={
+                // {".................................................."},
+                // {".................................................."},
+                // {".................................................."},
+                // {".................................................."},
+                // {".................................................."},
+                // {".................................................."},
+                // {".................................................."},
+                
+                {"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
+                {"X................................................X"},
+                {"X................................................X"},
+                {"X................................................X"},
+                {"X................................................X"},
+                {"X................................................X"},
+                {"X................................................X"},
+                {"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
+                };
+
+    foreach (String s1 in s)
+    {
+        Console.WriteLine(s1);
+    }
+}
+moveSimbol();
+
---
+++ Example0019_dataTime/Program.cs
@@ -0,0 +1,254 @@
+﻿
+String[,] s0 ={
+{"0000"},
+{"0  0"},
+{"0  0"},
+{"0  0"},
+{"0000"},};
+String[,] s1 ={
+{"   0"},
+{"  00"},
+{" 0 0"},
+{"   0"},
+{"   0"},};
+String[,] s2 ={
+{"0000"},
+{"   0"},
+{"0000"},
+{"0   "},
+{"0000"},
+};
+String[,] s3 ={
+{"0000"},
+{"   0"},
+{"0000"},
+{"   0"},
+{"0000"},
+};
+String[,] s4 ={
+{"0  0"},
+{"0  0"},
+{"0000"},
+{"   0"},
+{"   0"},
+};
+String[,] s5 ={
+{"0000"},
+{"0   "},
+{"0000"},
+{"   0"},
+{"0000"},
+};
+String[,] s6 ={
+{"0000"},
+{"0   "},
+{"0000"},
+{"0  0"},
+{"0000"},
+};
+String[,] s7 ={
+{"0000"},
+{"   0"},
+{"  0 "},
+{" 0  "},
+{"0   "},
+};
+String[,] s8 ={
+{"0000"},
+{"0  0"},
+{"0000"},
+{"0  0"},
+{"0000"},
+};
+String[,] s9 ={
+{"0000"},
+{"0  0"},
+{"0000"},
+{"   0"},
+{"0000"},
+};
+
+DateTime dateTime = new DateTime();
+dateTime = DateTime.Now;
+int h1 = 0;
+int h2 = 0;
+int min1 = 1;
+int min2 = 2;
+String[,] mas1 = s0;
+String[,] mas2 = s0;
+String[,] mas3 = s0;
+String[,] mas4 = s0;
+while (true)
+{
+    //Console.Clear();
+    // dateTime = DateTime.Now;
+    // Console.Write($"{dateTime.Hour}:{dateTime.Minute}:{dateTime.Second} ");
+
+    dateTime = DateTime.Now;
+    h1 = dateTime.Hour / 10;
+    h2 = dateTime.Hour % 10;
+    min1 = dateTime.Minute / 10;
+    min2 = dateTime.Minute % 10;
+    for (int i = 0; i < 5; i++)
+    {
+        switch (h1)
+        {
+            case 0:
+                mas1 = s0;
+                break;
+            case 1:
+                mas1 = s1;
+                break;
+            case 2:
+                mas1 = s2;
+                break;
+            case 3:
+                mas1 = s3;
+                break;
+            case 4:
+                mas1 = s4;
+                break;
+            case 5:
+                mas1 = s5;
+                break;
+            case 6:
+                mas1 = s6;
+                break;
+            case 7:
+                mas1 = s7;
+                break;
+            case 8:
+                mas1 = s8;
+                break;
+            case 9:
+                mas1 = s9;
+                break;
+        }
+        switch (h2)
+        {
+            case 0:
+                mas2 = s0;
+                break;
+            case 1:
+                mas2 = s1;
+                break;
+            case 2:
+                mas2 = s2;
+                break;
+            case 3:
+                mas2 = s3;
+                break;
+            case 4:
+                mas2 = s4;
+                break;
+            case 5:
+                mas2 = s5;
+                break;
+            case 6:
+                mas2 = s6;
+                break;
+            case 7:
+                mas2 = s7;
+                break;
+            case 8:
+                mas2 = s8;
+                break;
+            case 9:
+                mas2 = s9;
+                break;
+        }
+        switch (min1)
+        {
+            case 0:
+                mas3 = s0;
+                break;
+            case 1:
+                mas3 = s1;
+                break;
+            case 2:
+                mas3 = s2;
+                break;
+            case 3:
+                mas3 = s3;
+                break;
+            case 4:
+                mas3 = s4;
+                break;
+            case 5:
+                mas3 = s5;
+                break;
+            case 6:
+                mas3 = s6;
+                break;
+            case 7:
+                mas3 = s7;
+                break;
+            case 8:
+                mas3 = s8;
+                break;
+            case 9:
+                mas3 = s9;
+                break;
+        }
+        switch (min2)
+        {
+            case 0:
+                mas4 = s0;
+                break;
+            case 1:
+                mas4 = s1;
+                break;
+            case 2:
+                mas4 = s2;
+                break;
+            case 3:
+                mas4 = s3;
+                break;
+            case 4:
+                mas4 = s4;
+                break;
+            case 5:
+                mas4 = s5;
+                break;
+            case 6:
+                mas4 = s6;
+                break;
+            case 7:
+                mas4 = s7;
+                break;
+            case 8:
+                mas4 = s8;
+                break;
+            case 9:
+                mas4 = s9;
+                break;
+        }
+        Console.Write(mas1[i, 0]);
+        Console.Write("  ");
+        Console.Write(mas2[i, 0]);
+        Console.Write("  ");
+        Console.Write("|");
+        Console.Write("  ");
+        Console.Write(mas3[i, 0]);
+        Console.Write("  ");
+        Console.Write(mas4[i, 0]);
+        Console.WriteLine();
+    }
+    Thread.Sleep(3000);
+    Console.Clear();
+
+
+
+
+}
+void printBoard(string[,] num)
+{
+    foreach (String s1 in num)
+    {
+        Console.WriteLine(s1);
+    }
+}
+
+
+
+
